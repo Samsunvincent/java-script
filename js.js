5a8a8b09bd91189a662c67bce8fb3445a2870007
samsun <samsunvincent143@gmail.com>
2024-08-14 11:30:18 +0530

    added regular expression

--- js.js
+++ js.js
@@ -551,123 +551,195 @@
 
 
 
-console.log('\n\n\n\n\n\n\n\n')
-// call back queue request
-{
-    let xhr = new XMLHttpRequest();
-    console.log('xhr', xhr);
-    xhr.open('get', 'https://jsonplaceholder.typicode.com/users');
-    document.getElementById('btn').onclick = function () {
-        xhr.send();
+// console.log('\n\n\n\n\n\n\n\n')
+// // call back queue request
+// {
+//     let xhr = new XMLHttpRequest();
+//     console.log('xhr', xhr);
+//     xhr.open('get', 'https://jsonplaceholder.typicode.com/users');
+//     document.getElementById('btn').onclick = function () {
+//         xhr.send();
 
-    }
-    xhr.onreadystatechange = function () {
-        console.log("ready state :  ", xhr.readyState);
-        if (xhr.readyState === 4) {
-            console.log('status', xhr.status)
-            if (xhr.status === 200) {
-                let response = xhr.response;
-                console.log('response : ', response)
-                console.log('type of response : ', typeof (response));
-
-
-                let datas = JSON.parse(response);
-                console.log('datas : ', datas)
-
-                let datacontainer = document.getElementById('data-container');
-                console.log('data container', datacontainer);
-
-                let rows = '';
-                for (let i = 0; i < datas.length; i++) {
-                    rows = rows + `
-                <tr>
-                <td>${datas[i].name}</td>
-                <td>${datas[i].username}</td>
-                <td>${datas[i].email}</td>
-                <td>${datas[i].website}</td>
-                <td><button onclick = "handleClick(${datas[i].id})">View</button></td>
-                </tr>
-
-                `
-                }
-                console.log(("rows", rows));
-
-                datacontainer.innerHTML = rows;
-            }
-        }
-    }
-    function handleClick(id) {
-        console.log("reached here");
-        console.log("id : ", id)
-        window.location.href = "dashboard.html"
-    }
-    function loaduserdata() {
-        console.log("sdfghjkl")
+//     }
+//     xhr.onreadystatechange = function () {
+//         console.log("ready state :  ", xhr.readyState);
+//         if (xhr.readyState === 4) {
+//             console.log('status', xhr.status)
+//             if (xhr.status === 200) {
+//                 let response = xhr.response;
+//                 console.log('response : ', response)
+//                 console.log('type of response : ', typeof (response));
 
 
-    }
-}
+//                 let datas = JSON.parse(response);
+//                 console.log('datas : ', datas)
+
+//                 let datacontainer = document.getElementById('data-container');
+//                 console.log('data container', datacontainer);
+
+//                 let rows = '';
+//                 for (let i = 0; i < datas.length; i++) {
+//                     rows = rows + `
+//                 <tr>
+//                 <td>${datas[i].name}</td>
+//                 <td>${datas[i].username}</td>
+//                 <td>${datas[i].email}</td>
+//                 <td>${datas[i].website}</td>
+//                 <td><button onclick = "handleClick(${datas[i].id})">View</button></td>
+//                 </tr>
+
+//                 `
+//                 }
+//                 console.log(("rows", rows));
+
+//                 datacontainer.innerHTML = rows;
+//             }
+//         }
+//     }
+//     function handleClick(id) {
+//         console.log("reached here");
+//         console.log("id : ", id)
+//         window.location.href = "dashboard.html"
+//     }
+//     function loaduserdata() {
+//         console.log("sdfghjkl")
 
 
-{
-
-    let datas = [
-        {
-            id: 1,
-            name: "john",
-            age: 12,
-        },
-        {
-            id: 2,
-            name: "Doe",
-            age: 25,
-        }
-    ];
-    console.log("datas", datas);
-
-    function findUser(id) {
-        let user = datas.find(data => data.id === id);
-        return user;
-    }
-
-    let user = findUser(1);
-    console.log("user", user)
-
-    function findUser1(id) {
-        let user = datas.find(data => data.id === id);
-        setTimeout(() => {
-            return user;
-        }, 1000);
-    }
-    let user1 = findUser1(2)
-    console.log('user1', user1)
+//     }
+// }
 
 
-    function findUser2(id){
-       return new Promise((resolve,reject)=>{
-        let user = datas.find(data => data.id === id);
-        setTimeout(() => {
-            if(user){
-                resolve(user);
-            }
-            else{
-                reject("user not found")
-            }
-        }, 2000);
-       })
+// {
+
+//     let datas = [
+//         {
+//             id: 1,
+//             name: "john",
+//             age: 12,
+//         },
+//         {
+//             id: 2,
+//             name: "Doe",
+//             age: 25,
+//         }
+//     ];
+//     console.log("datas", datas);
+
+//     function findUser(id) {
+//         let user = datas.find(data => data.id === id);
+//         return user;
+//     }
+
+//     let user = findUser(1);
+//     console.log("user", user)
+
+//     function findUser1(id) {
+//         let user = datas.find(data => data.id === id);
+//         setTimeout(() => {
+//             return user;
+//         }, 1000);
+//     }
+//     let user1 = findUser1(2)
+//     console.log('user1', user1)
+
+
+//     function findUser2(id){
+//        return new Promise((resolve,reject)=>{
+//         let user = datas.find(data => data.id === id);
+//         setTimeout(() => {
+//             if(user){
+//                 resolve(user);
+//             }
+//             else{
+//                 reject("user not found")
+//             }
+//         }, 2000);
+//        })
     
-}
-findUser2(1)
-.then((user)=>{
-    console.log("user2",user);
-})
-.catch((error)=>{
-    console.log("error from catch",error)
-})
-}
-
+// }
+// findUser2(1)
+// .then((user)=>{
+//     console.log("user2",user);
+// })
+// .catch((error)=>{
+//     console.log("error from catch",error)
+// })
+// }
 
 // async functions
+
+// regular expressions
+
+const str1 = "The Cat is White"
+const str2 = "rats  2 eats fish"
+const str3 = "is it okay fishe"
+const str4 = `hello
+good meat
+is always good
+Hai`;
+
+
+const regexpr1 = /a/;
+const result1 = regexpr1.test(str1)
+console.log("result1",result1); //true
+
+const regexpr2 = /A/i; //case insensitive
+const result2 = regexpr2.test(str1);
+console.log('result2',result2); //true
+
+const regexpr3 = /[c,b,r]a/ //to check wether the letter before a contains c,b or a
+const result3 = regexpr3.test(str2);
+console.log('result3',result3)//true
+
+const regexpr4 = /[0-9]/
+const result4 = regexpr4.test(str2);
+console.log('result4',result4) //true
+
+// ^ represents start of a string
+const regexpr5 = /^good/im;
+const result5 = regexpr5.test(str4);
+console.log('result5',result5) //true
+
+// $ represents ending of a string
+
+const regexpr6 = /hai$/im
+const result6 = regexpr6.test(str4);
+console.log('result6 : ',result6);
+
+const regexpr7 = /^hai/im
+const result7 = regexpr7.test(str4);
+console.log('result7 : ',result7);//true
+
+
+const str5 ="Hello"
+const regexpr8 = /[kjm]/i
+const result8 = regexpr8.test(str5);
+console.log(result8) //false
+
+const regexpr9 = /[^kjm]/i // ^ here it is used to make sure that there is no k,j or m
+const result9 = regexpr9.test(str5);
+
+console.log(result9);
+
+const regexpr10 = /h+/i //atleast one h is required
+const result10 = regexpr10.test(str5);
+console.log('result10',result10)
+
+const regexpr11 = /h$/i
+const result11 = regexpr11.test(str5);
+console.log(result11) //false
+
+const regexpr12 = /[h?$]/i
+const result12 = regexpr12.test(str5);
+console.log('result12',result12)
+
+
+
+
+
+
+
+
 
 
 
